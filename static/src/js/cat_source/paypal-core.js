5401e03bc29908f0f46f3611705d390c77b8c3be
riccio82 <riccio82@gmail.com>
2017-10-16 12:26:39 +0200

    Footer tab Messages

--- static/src/js/cat_source/paypal-core.js
+++ static/src/js/cat_source/paypal-core.js
@@ -19,6 +19,9 @@ let interact = require('interactjs');
             let self  = this;
             originalSetEvents.apply(this);
 
+            // To make tab Footer messages opened by default
+            // SegmentActions.registerTab('messages', true, true);
+
             this.createPreviewContainer();
 
             Store.addListener(Constants.SELECT_SEGMENT, this.selectSegment.bind(this));
