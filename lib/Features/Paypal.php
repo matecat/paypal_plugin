496037bdf62b604fbf0e5051bcf805eaf6cbd702
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-01-09 18:33:26 +0100

    manage project type with redirect

--- lib/Features/Paypal.php
+++ lib/Features/Paypal.php
@@ -22,6 +22,7 @@ use Features\Paypal\Controller\LqaController;
 use Features\Paypal\Utils\CDataHandler;
 use Klein\Klein;
 use viewController;
+use Projects_MetadataDao;
 
 class Paypal extends BaseFeature {
 
@@ -228,6 +229,40 @@ class Paypal extends BaseFeature {
             \Log::doLog( "TODO Implements Authentication exception behaviour" );
             trigger_error( "TODO Implements Authentication exception behaviour", E_USER_WARNING );
             die();
+
+        }
+
+
+    }
+
+    public function manageProjectType( viewController $controller ) {
+
+        if ( $controller instanceof \catController ) {
+            $project      = $controller->project;
+            $metadata     = new Projects_MetadataDao;
+            $project_type = $metadata->get( $project->id, "project_type" );
+            if ( !empty( $project_type ) ) {
+                if ( $controller->isRevision() ) {
+                    $page = "revision";
+                } else {
+                    $page = "translation";
+                }
+
+                if ( $project_type->value == "TR" && $page == "revision" ) {
+                    $chunk = $controller->getChunk();
+                    header( 'Location: ' . \Routes::translate( $project->name, $chunk->id, $chunk->password, $chunk->source, $chunk->target ) );
+                    //sleep(1);
+                }
+
+                if ( $project_type->value == "LR" && $page == "translation" ) {
+                    $chunk = $controller->getChunk();
+                    $job   = \LQA\ChunkReviewDao::findByIdJob( $chunk->id );
+                    header( 'Location: ' . \Routes::revise( $project->name, $chunk->id, $job[ 0 ]->review_password, $chunk->source, $chunk->target ) );
+                    //sleep(1);
+                }
+
+
+            }
 
         }
 
