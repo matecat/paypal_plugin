1c390226c34b4ed39c0ff17d516052d261f585f3
riccio82 <riccio82@gmail.com>
2018-03-08 16:04:10 +0100

    Fix MaC

--- static/src/js/cat_source/paypal-core.extensions.js
+++ static/src/js/cat_source/paypal-core.extensions.js
@@ -19,7 +19,7 @@ let Store = require('../store/PreviewsStore');
     var original_addIssuesToSegment = UI.addIssuesToSegment;
     var original_deleteSegmentIssues = UI.deleteSegmentIssues;
     var originalGotoNextSegment = UI.gotoNextSegment;
-
+    var originalisMarkedAsCompleteClickable = UI.isMarkedAsCompleteClickable;
     $.extend(UI, {
 
         scrollSelector: "#outer",
