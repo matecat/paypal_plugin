0820a2fd6441593f9b92eb1058cc96d47d96f9ad
riccio82 <riccio82@gmail.com>
2018-03-28 16:54:25 +0200

    Analisys page: show revise link if translate completed

--- static/src/js/cat_source/paypal-analyze.js
+++ static/src/js/cat_source/paypal-analyze.js
@@ -27,6 +27,18 @@
                     "/plugins/paypal/lqa/" + this.props.outsourceJobId + "/" + this.props.job.get('review_password');
             } else if (projectType && projectType === 'LR') {
                 url = url.replace('/translate/' , '/revise/');
+            } else {
+                let project_status = this.props.project.get('project_status');
+                if ( !projectType &&  project_status) {
+                    let self = this;
+                    let translateJobStatus = project_status.get('translate').find(function ( item ) {
+                        return parseInt(item.get('id')) === parseInt(self.props.outsourceJobId);
+                    });
+                    if ( translateJobStatus && translateJobStatus.get('completed') ) {
+                        url =  window.location.protocol + '//' + window.location.host + '/revise/'+ this.props.project.get('project_slug')+'/'+ this.props.job.get('source') +'-'+ this.props.job.get('target')+'/'
+                            + this.props.outsourceJobId +'-'+ this.props.job.get('review_password')  ;
+                    }
+                }
             }
             return url;
 
@@ -41,14 +53,57 @@
             } else if (projectType && projectType === 'LR') {
                 url =  '/revise/'+ this.props.project.get('project_slug')+'/'+ this.props.job.get('source') +'-'+ this.props.job.get('target')+'/'
                     + this.props.outsourceJobId +'-'+ this.props.job.get('review_password')  ;
+            } else {
+                let project_status = this.props.project.get('project_status');
+                if ( !projectType &&  project_status) {
+                    let self = this;
+                    let translateJobStatus = project_status.get('translate').find(function ( item ) {
+                        return parseInt(item.get('id')) === parseInt(self.props.outsourceJobId);
+                    });
+                    if ( translateJobStatus && translateJobStatus.get('completed') ) {
+                        url =  '/revise/'+ this.props.project.get('project_slug')+'/'+ this.props.job.get('source') +'-'+ this.props.job.get('target')+'/'
+                            + this.props.outsourceJobId +'-'+ this.props.job.get('review_password')  ;
+                    }
+                }
             }
             return url;
 
         };
 
-        return AnalyzeHeader;
+        return OpenJobBox;
+    }
+
+    function overrideJobOpenButton(AnalyzeChunksResume) {
+        let original_getOpenButton = AnalyzeChunksResume.prototype.getOpenButton;
+        AnalyzeChunksResume.prototype.getOpenButton = function (chunkJob, index ) {
+            let project_type = this.props.project.get('project_type');
+            let project_status = this.props.project.get('project_status');
+            if ( !project_type &&  project_status) {
+                let translateJobStatus = project_status.get('translate').find(function ( item ) {
+                    return parseInt(item.get('id')) === parseInt(chunkJob.get('id'));
+                });
+                if ( translateJobStatus && translateJobStatus.get('completed') ) {
+                    return <div className="open-revise ui green button open"
+                                onClick={this.openOutsourceModal.bind(this, index)}>Revise</div>;
+                }
+            }
+            return original_getOpenButton.apply(this, [chunkJob, index])
+        }
     }
 
     overrideAnalyzeHeaderFn(AnalyzeHeader);
     overrideJobLinkFn(OpenJobBox);
+    overrideJobOpenButton(AnalyzeChunksResume);
+
+    var original_renderAnalysisPage = UI.renderAnalysisPage;
+
+    $.extend(UI, {
+        renderAnalysisPage: function (  ) {
+            original_renderAnalysisPage.apply(this);
+            API.PROJECTS.getCompletionStatus().done(function ( data ) {
+                UI.currentOutsourceProject.project_status = data.project_status;
+                AnalyzeActions.updateProject(UI.currentOutsourceProject);
+            })
+        }
+    });
 })() ;
