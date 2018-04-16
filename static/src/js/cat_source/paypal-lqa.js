23b3fad2eb0ffda7ab594f4a28dd3a2aa1840282
riccio82 <riccio82@gmail.com>
2018-04-16 10:28:44 +0200

    Refactoring: createButtons

--- static/src/js/cat_source/paypal-lqa.js
+++ static/src/js/cat_source/paypal-lqa.js
@@ -2,17 +2,17 @@
 
 
 (function() {
-    let originalOverrideButtonsForRevision = UI.overrideButtonsForRevision;
-    $.extend(UI, {
-        overrideButtonsForRevision: function () {
-            var div = $('<ul>' + UI.segmentButtons + '</ul>');
-
-            div.find('.translated').text('APPROVED').removeClass('translated').addClass('approved');
-            var nextSegment = UI.currentSegment.next();
-            var goToNextApprovedButton = !nextSegment.hasClass('status-translated');
-            div.find('.next-untranslated').parent().remove();
-            UI.segmentButtons = div.html();
-        }
-    });
+    // let originalOverrideButtonsForRevision = UI.overrideButtonsForRevision;
+    // $.extend(UI, {
+    //     overrideButtonsForRevision: function () {
+    //         var div = $('<ul>' + UI.segmentButtons + '</ul>');
+    //
+    //         div.find('.translated').text('APPROVED').removeClass('translated').addClass('approved');
+    //         var nextSegment = UI.currentSegment.next();
+    //         var goToNextApprovedButton = !nextSegment.hasClass('status-translated');
+    //         div.find('.next-untranslated').parent().remove();
+    //         UI.segmentButtons = div.html();
+    //     }
+    // });
 
 })() ;
